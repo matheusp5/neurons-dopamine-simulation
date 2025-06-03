4387c5dfa22be096ac280dba4d0b4cb272546826
matheusp5 <mxtheuzchika@gmail.com>
2025-06-02 22:49:05 -0300

    feat: implement dopamine presence check and enhance action execution

--- main.py
+++ main.py
@@ -2,17 +2,19 @@ from enum import Enum
 from time import sleep
 import threading  
 
-# maybe there will have a thread just for checking if
-# there are dopamine in receptors
+# check dopamine present in dopamine receptor instead of neuron
+
+DOPAMINE_RELEASE_AMOUNT = 1
 
 class ActionType(Enum):
   play_with_friends = 'play_with_friends'
 
 class Action:
-  def __init__(self, action_type: ActionType):
+  def __init__(self, action_type: ActionType, increased_dopamine_release: int, time_to_call_dat_transporters: int):
+    self.increased_dopamine_release = increased_dopamine_release
+    self.time_to_call_dat_transporters = time_to_call_dat_transporters
     self.action_type = action_type
     pass
-
 
 class Excitable:  
   excitement_level = 0
@@ -32,10 +34,11 @@ class DopamineReceptor:
   def __init__(self, neuron: Excitable):
     self.neuron = neuron
   
-  def trigger_dopamine_release(self, time = 1):
-    print('Dopamine released')
+  def trigger_dopamine_release(self, time_to_call_dat_transporters: int = 5):
     self.dopamine_presence = True
-    sleep(time)
+
+    # trigger DAT transporter for dopamine reuptake
+    sleep(time_to_call_dat_transporters)
     self.dopamine_presence = False
     
     
@@ -56,15 +59,27 @@ class Neuron(Excitable):
       self.d1_dopamine_receptors = d1_dopamine_receptors
       self.d2_dopamine_receptors = d2_dopamine_receptors
 
-  def trigger_potential_dopamine_action(self):
+  def trigger_potential_dopamine_action(self, action: Action):
+    for receptor in self.d1_dopamine_receptors:
+      threading.Thread(target=receptor.trigger_dopamine_release, args=(action.time_to_call_dat_transporters,)).start()
 
-    pass
+    for receptor in self.d2_dopamine_receptors:
+      threading.Thread(target=receptor.trigger_dopamine_release, args=(action.time_to_call_dat_transporters,)).start()
 
   def add_d1_dopamine_receptor(self, receptor: D1DopamineReceptor):
     self.d1_dopamine_receptors.append(receptor)
 
   def add_d2_dopamine_receptor(self, receptor: D2DopamineReceptor):
     self.d2_dopamine_receptors.append(receptor)
+
+  def check_dopamine_presence(self, action: Action) -> bool:
+    for receptor in self.d1_dopamine_receptors + self.d2_dopamine_receptors:
+      if receptor.dopamine_presence:
+        dopamine_to_increase = DOPAMINE_RELEASE_AMOUNT * action.increased_dopamine_release
+        self.increase_excitement(dopamine_to_increase)
+      else:
+        self.decrease_excitement(DOPAMINE_RELEASE_AMOUNT)
+    
 
 class Brain:
   neurons = []
@@ -76,31 +91,9 @@ class Brain:
     self.neurons.append(neuron)
 
   def calculate_total_dopamine(self) -> int:
-    return sum(neuron.calculate_total_dopamine() for neuron in self.neurons)
+    return sum(neuron.excitement_level for neuron in self.neurons)
   
-  def execute_action(self, action):
-    pass
-    
-def check_dopamine_for_receptor(receptor: DopamineReceptor):
-  print('Checking dopamine presence for receptor...')
+  def execute_action(self, action: Action):
+    for neuron in self.neurons:
+      neuron.trigger_potential_dopamine_action(action)
 
-  while(True):
-    if receptor.dopamine_presence:
-      print('Has dopamine')
-    else:
-      print('No dopamine')
-    sleep(1)
-
-
-# tests
-
-dopamine_receptor = D1DopamineReceptor()
-
-thread = threading.Thread(target=check_dopamine_for_receptor,args=(dopamine_receptor,))
-
-thread.start()
-
-
-sleep(2)
-
-dopamine_receptor.trigger_dopamine_release()
